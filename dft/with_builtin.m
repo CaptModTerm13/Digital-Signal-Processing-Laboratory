8d9f2cab9e013ece365c50d29f50d8908ace2deb
Dixon Mathews <118825481+CaptModTerm13@users.noreply.github.com>
2024-10-03 12:42:10 +0530

    added plot for dft

--- dft/with_builtin.m
+++ dft/with_builtin.m
@@ -3,7 +3,13 @@ disp('dft of x[n] is');
 X_fft=fft(x);
 disp(X_fft);
 figure;
-stem(abs(X_fft));
-xlabel('Time scale');
+subplot(2,1,1);
+stem(0:length(X_fft)-1,abs(X_fft));
+xlabel('frequency');
 ylabel('Magnitude');
 title('DFT of x');
+subplot(2,1,2);
+stem(0:length(X_fft)-1,angle(X_fft),"filled");
+xlabel('frequency');
+ylabel('phase (rads)');
+title('phase spectrum')
